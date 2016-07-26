06f147080edd9ec94d82377ebe617d5534f44265
Braeden Atlee <nedearb@gmail.com>
2016-07-26 15:11:30 -0700

    Initial

---
+++ src/org/usfirst/frc/team1492/robot/Robot.java
@@ -0,0 +1,29 @@
+
+package org.usfirst.frc.team1492.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+    
+    public void robotInit() {
+    	
+    }
+    
+    public void autonomousInit() {
+    	
+    }
+
+    
+    public void autonomousPeriodic() {
+    	
+    }
+    
+    public void teleopPeriodic() {
+        
+    }
+    
+    public void testPeriodic() {
+    	
+    }
+    
+}
