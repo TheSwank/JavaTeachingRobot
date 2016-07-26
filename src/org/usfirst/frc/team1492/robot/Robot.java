21076bbd55624939be072a64641d057f985649de
Braeden Atlee <nedearb@gmail.com>
2016-07-26 15:30:50 -0700

    added joysticks

--- src/org/usfirst/frc/team1492/robot/Robot.java
+++ src/org/usfirst/frc/team1492/robot/Robot.java
@@ -2,11 +2,16 @@
 package org.usfirst.frc.team1492.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 
 public class Robot extends IterativeRobot {
+
+	Joystick joystickLeft;
+	Joystick joystickRight;
     
     public void robotInit() {
-    	
+    	joystickLeft = new Joystick(0);
+    	joystickRight = new Joystick(1);
     }
     
     public void autonomousInit() {
